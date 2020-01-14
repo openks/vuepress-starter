b3c98e4245d2e71da0e164bc1e50d16737eac53d
openks <zhuyangxing@foxmail.com>
2020-01-14 14:56:13 +0800

    添加最后更新时间

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 
-var fs = require('fs')
-
+const fs = require('fs')
+const moment = require('moment');
 const path = require("path");
 
 //  遍历目录得到文件信息
@@ -19,10 +19,19 @@ function walk(path2) {
 module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
+    plugins: [
+        '@vuepress/last-updated',
+        {
+            transformer: (timestamp, lang) => {
+                // 不要忘了安装 moment
+                return moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
+            }
+        }],
     themeConfig: {
-        plugins: ['@vuepress/last-updated'],
-        lastUpdated: 'Last Updated',
+        lastUpdated: '上次更新',
         nav: [
+            { text: 'Home', link: '/' },
+            { text: 'blog', link: '/blog/' },
             { text: '百度', link: 'http://www.baidu.com' }
         ],
         sidebar: [
